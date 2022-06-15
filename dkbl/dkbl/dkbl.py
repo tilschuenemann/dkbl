950a199c19b5c65bc5ce18de2eb3b48419624032
schuenemann <til.schuenemann@mailbox.org>
2022-06-15 17:18:13 +0200

    add distribute-occurences

--- dkbl/dkbl/dkbl.py
+++ dkbl/dkbl/dkbl.py
@@ -361,3 +361,62 @@ def update_ledger_mappings(output_folder: pathlib.Path) -> pd.DataFrame:
     _write_ledger_to_disk(ledger, output_folder, "ledger")
 
     return ledger
+
+
+def distribute_occurences(output_folder: pathlib.Path) -> pd.DataFrame:
+    """Reads the ledger from the output_folder and creates timeseries
+    for all line items that have an occurence that is not 1, 0 or -1.
+    All their amounts get divided by the occurence and the dates get set to the
+    start of that month.
+
+    Parameters
+    -------
+
+    Returns
+    -------
+
+    """
+
+    df = _handle_import(output_folder, "ledger")
+
+    if not (set(df).issuperset(["date", "amount", "occurence"])) or df.shape[0] == 0:
+        exit("malformed input df")
+
+    # TODO coalesce occurence_custom
+
+    mask = df["occurence"].between(-1, 1, inclusive="both")
+    no_rep = df[mask]
+    rep = df[~mask]
+
+    # create new dates, which will get appended later
+    new_dates = pd.DataFrame()
+
+    for row in rep.itertuples():
+        date = row.date
+        n = row.occurence
+
+        if n > 0:
+            tmp = pd.DataFrame(
+                pd.date_range(start=date, periods=n, freq="MS").tolist(),
+                columns=["date"],
+            )
+        else:
+            tmp = pd.DataFrame(
+                pd.date_range(end=date, periods=abs(n), freq="MS").tolist(),
+                columns=["date"],
+            )
+        new_dates = pd.concat([new_dates, tmp], axis=0, ignore_index=True)
+
+    # repeat rows by occurence value and add new dates
+    rep = rep.reset_index(drop=True)
+    rep = rep.reindex(rep.index.repeat(abs(rep["occurence"])))
+    rep = rep.reset_index(drop=True)
+    rep["amount"] = rep["amount"] / abs(rep["occurence"])
+    rep["date"] = new_dates["date"]
+
+    dis = pd.concat([no_rep, rep], axis=0)
+    dis["date"] = pd.to_datetime(dis["date"], format="%Y-%m-%d")
+
+    _write_ledger_to_disk(dis, output_folder, "dist_leder")
+
+    return dis
